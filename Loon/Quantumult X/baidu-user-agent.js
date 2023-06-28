9bfca5a3b7bfb492a5541596fd7d419f381097bc
Essaim8 <127705161+Essaim8@users.noreply.github.com>
2023-06-28 21:46:49 +0800

    Delete Loon/Quantumult X directory

--- Loon/Quantumult X/baidu-user-agent.js
+++
@@ -1,5 +0,0 @@
-var modifiedHeaders = $request.headers;
-
-modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SearchCraft/3.9.0 (Baidu; P2 16.3)';
-
-$done({ headers: modifiedHeaders });
